cd5b3e3eeb827a47d423bc9dc20d8613f134baba
Ron <ron@rapitec.co.il>
2020-07-03 13:00:12 +0300

    first code

--- src/008-hello-world/anything.go
+++ src/008-hello-world/anything.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main () {
-	fmt.Println("Hello class! test string");
+func main() {
+	fmt.Println("Hello class! test string")
+	foo()
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+}
+
+func foo() {
+	fmt.Println("Second string")
+
+	bar()
+}
+
+func bar() {
+	fmt.Println("Third string")
 }
