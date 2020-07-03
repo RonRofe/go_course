d3f8da1ff89c63cb5483ab43c012353ce1378550
Ron <ron@rapitec.co.il>
2020-07-03 19:34:29 +0300

    string type in depth

--- src/types.go
+++ src/types.go
@@ -4,22 +4,22 @@ import (
 	"fmt"
 )
 
-var boo bool
-var in int
-var flo float64
-var in8 int8
-var same1 uint8
-var same2 byte
-var oto1 int32
-var oto2 rune
-
 func main() {
-	fmt.Println(y)
-	fmt.Printf("%T\n", y)
-	fmt.Println(z)
-	fmt.Printf("%T\n", z)
-	fmt.Println(a)
-	fmt.Printf("%T\n", a)
-	fmt.Println("Initial value of string: ", defStr, "!")
-	fmt.Println("Initial value of integer: ", defInt, "!")
+	s := "Hello, playground"
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+
+	bs := []byte(s)
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%#U ", s[i])
+	}
+
+	fmt.Println();
+	
+	for i, v := range s {
+		fmt.Printf("At index position: %d we have hex %#x\n", i, v)
+	}
 }
