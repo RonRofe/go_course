c45cc1cb24d4d7985e7c7be9ca304cdc01d97e45
Ron <ron@rapitec.co.il>
2020-07-03 14:35:44 +0300

    new line in string

--- src/types.go
+++ src/types.go
@@ -6,10 +6,15 @@ import (
 
 var y = 42
 var z = "test of Z"
+var a = `James said
+
+"Hello world"`
 
 func main() {
 	fmt.Println(y)
 	fmt.Printf("%T\n", y)
 	fmt.Println(z)
 	fmt.Printf("%T\n", z)
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
 }
