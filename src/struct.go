95d8e5fed9b529e7c94394d5cc7ba9b52d94561e
Ron <ron@rapitec.co.il>
2020-07-04 14:59:38 +0300

    anonymous struct

--- src/struct.go
+++ src/struct.go
@@ -4,32 +4,38 @@ import (
 	"fmt"
 )
 
-type person struct {
-	first string
-	last  string
-	age   int
-}
+// type person struct {
+// 	first string
+// 	last  string
+// 	age   int
+// }
 
-type secretAgent struct {
-	person
-	ltk bool
-}
+// type secretAgent struct {
+// 	person
+// 	ltk bool
+// }
 
 func main() {
-	sa1 := secretAgent{
-		person: person{
-			first: "James",
-			last:  "Bond",
-			age:   46,
-		},
-		ltk: false,
-	}
-
-	// p2 := person{
-	// 	first: "John",
-	// 	last:  "Cina",
-	// 	age:   34,
+	// sa1 := secretAgent{
+	// 	person: person{
+	// 		first: "James",
+	// 		last:  "Bond",
+	// 		age:   46,
+	// 	},
+	// 	ltk: false,
 	// }
 
-	fmt.Println(sa1.first, sa1.last, sa1.person.age, sa1.ltk)
+	p2 := struct {
+		first string
+		last  string
+		age   int
+	}{
+		first: "John",
+		last:  "Cina",
+		age:   34,
+	}
+
+	fmt.Println(p2)
+
+	// fmt.Println(sa1.first, sa1.last, sa1.person.age, sa1.ltk)
 }
