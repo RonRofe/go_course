5c46a0e8f0f735592846f4caf4fb8eb083802b14
Ron <ron@rapitec.co.il>
2020-07-04 10:59:34 +0300

    multi dimensional slice

--- src/slice.go
+++ src/slice.go
@@ -5,6 +5,14 @@ import (
 )
 
 func main() {
+	jb := []string{"James", "Bond", "Martini"}
+	fmt.Println(jb)
+	mp := []string{"This", "Map", "Three"}
+	fmt.Println(jb)
+
+	xp := [][]string{jb, mp}
+
+	fmt.Println(xp)
 	// x := []int{1, 3, 4, 5}
 
 	// fmt.Println(x)
@@ -30,9 +38,9 @@ func main() {
 	// x = append(x[:2], x[4:]...)
 	// fmt.Println(x)
 
-	x := make([]int, 10, 100)
+	// x := make([]int, 10, 100)
 
-	fmt.Println(x)
-	fmt.Println(len(x))
-	fmt.Println(cap(x))
+	// fmt.Println(x)
+	// fmt.Println(len(x))
+	// fmt.Println(cap(x))
 }
