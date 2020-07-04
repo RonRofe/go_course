6013fae31b9d65662345368fda42c410695e1eb8
Ron <ron@rapitec.co.il>
2020-07-04 10:49:42 +0300

    delete items with append

--- src/slice.go
+++ src/slice.go
@@ -18,12 +18,15 @@ func main() {
 	// fmt.Println(x[1:])
 	// fmt.Println(x[1:3])
 
-	x = append(x, 77, 88, 99, 1014)
+	// x = append(x, 77, 88, 99, 1014)
 
-	fmt.Println(x)
+	// fmt.Println(x)
 
-	y := []int{0, 0, 0, 0}
+	// y := []int{0, 0, 0, 0}
 
-	x = append(x, y...)
+	// x = append(x, y...)
+	// fmt.Println(x)
+
+	x = append(x[:2], x[4:]...)
 	fmt.Println(x)
 }
