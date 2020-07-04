f3f89875ff4e054d41a53a355a053c4498bed027
Ron <ron@rapitec.co.il>
2020-07-04 10:42:46 +0300

    slice of slices

--- src/slice.go
+++ src/slice.go
@@ -7,11 +7,14 @@ import (
 func main() {
 	x := []int{1, 3, 4, 5}
 
-	fmt.Println(x)
-	fmt.Println(len(x))
-	fmt.Println(x[0])
+	// fmt.Println(x)
+	// fmt.Println(len(x))
+	// fmt.Println(x[0])
 
-	for i, v := range x {
-		fmt.Println(i, v)
-	}
+	// for i, v := range x {
+	// 	fmt.Println(i, v)
+	// }
+
+	fmt.Println(x[1:])
+	fmt.Println(x[1:3])
 }
