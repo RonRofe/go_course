2e22a98d84bbf5a6462ae02fe1e26fe2bd8d9e29
Ron <ron@rapitec.co.il>
2020-07-04 10:54:33 +0300

    make and cap functions

--- src/slice.go
+++ src/slice.go
@@ -5,9 +5,9 @@ import (
 )
 
 func main() {
-	x := []int{1, 3, 4, 5}
+	// x := []int{1, 3, 4, 5}
 
-	fmt.Println(x)
+	// fmt.Println(x)
 	// fmt.Println(len(x))
 	// fmt.Println(x[0])
 
@@ -27,6 +27,12 @@ func main() {
 	// x = append(x, y...)
 	// fmt.Println(x)
 
-	x = append(x[:2], x[4:]...)
+	// x = append(x[:2], x[4:]...)
+	// fmt.Println(x)
+
+	x := make([]int, 10, 100)
+
 	fmt.Println(x)
+	fmt.Println(len(x))
+	fmt.Println(cap(x))
 }
