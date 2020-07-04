c9778e0f63bb69fc6e0336323eab8661bfd0ff36
Ron <ron@rapitec.co.il>
2020-07-04 13:26:49 +0300

    different types in struct

--- src/struct.go
+++ src/struct.go
@@ -7,17 +7,20 @@ import (
 type person struct {
 	first string
 	last  string
+	age   int
 }
 
 func main() {
 	p1 := person{
 		first: "James",
 		last:  "Bond",
+		age:   48,
 	}
 
 	p2 := person{
 		first: "John",
 		last:  "Cina",
+		age:   34,
 	}
 
 	fmt.Println(p1.first, p2.last, p2.first, p2.last)
