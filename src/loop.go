e7297b2dc01834821a444f0030633d4dc4ee789f
Ron <ron@rapitec.co.il>
2020-07-04 09:20:26 +0300

    for as while loop

--- src/loop.go
+++ src/loop.go
@@ -5,10 +5,18 @@ import (
 )
 
 func main() {
-	for i := 0; i <= 10; i++ {
-		fmt.Printf("Out: %d\n", i)
-		for j := 0; j < 3; j++ {
-			fmt.Printf("\t\tIn: %d\n", j)
-		}
+	// for i := 0; i <= 10; i++ {
+	// 	fmt.Printf("Out: %d\n", i)
+	// 	for j := 0; j < 3; j++ {
+	// 		fmt.Printf("\t\tIn: %d\n", j)
+	// 	}
+	// }
+
+	x := 0
+	for x < 10 {
+		fmt.Printf("%x\t", x)
+		x++
 	}
+
+	fmt.Println()
 }
