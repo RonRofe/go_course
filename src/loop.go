ef33bf89afb06b7eeb98218faaa77ad6be4022b4
Ron <ron@rapitec.co.il>
2020-07-04 09:17:34 +0300

    order printing nesting loops

--- src/loop.go
+++ src/loop.go
@@ -6,8 +6,9 @@ import (
 
 func main() {
 	for i := 0; i <= 10; i++ {
+		fmt.Printf("Out: %d\n", i)
 		for j := 0; j < 3; j++ {
-			fmt.Printf("Out: %d/t Inner: %d\n", i, j)
+			fmt.Printf("\t\tIn: %d\n", j)
 		}
 	}
 }
